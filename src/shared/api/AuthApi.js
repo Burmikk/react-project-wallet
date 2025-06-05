a76493bc4de119a52b0c8585d15b291d6c8baddd
Burmikk <byrmik@gmail.com>
2025-06-05 17:49:17 +0200

    edit baseURL

--- src/shared/api/AuthApi.js
+++ src/shared/api/AuthApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https://wallet.b.goit.study/docs',
+  baseURL: 'https://wallet.b.goit.study',
 });
 
 const setToken = token => {
